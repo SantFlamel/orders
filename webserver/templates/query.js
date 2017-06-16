bd6feef9a4c7e684670ef6634d0ffc7c3e21699a
Linux Mordovin <i.pro.linux@gmail.com>
2017-06-16 18:55:35 +0500

    repare

---
+++ webserver/templates/query.js
@@ -0,0 +1,33 @@
+//----SERVICES
+"Printer"           - Печать
+"Promotions"        - Скидки Игорь
+"PromotionsTypes"   - Игорь
+"ProductOrder"      - Игорь
+"LocalTime"         - локальное время Заказы
+"ClientInfo"        - Игорь
+"ClientOrdersAddress"- Игорь
+"Session"           - Игорь
+"SessionInfo"       - Игорь
+"Tabel"             - Игорь
+"GetAreas"          - Евгений
+"GetPoint"          - Никита
+
+
+//----Продукты
+{"Tables":[{"Name":"ProductOrder","TypeParameter":"Range","Values":[ниже структуры],"Limit":0,"Offset":0}]}
+
+{"Table":"ProductOrder","Query":"Read"}
+{"Table":"PromotionsTypes","Query":"Read","Limit":999}
+{"Table":"Promotions","Query":"Read","Limit":999}
+{"Table":"Subjects","Query":"Read","Limit":999}
+{"Table":"ProductOrder","Query":"Read","TypeParameter":"TypeParameter","Values":["Value"]}")
+
+
+//----Продукты
+{"Tables":[{"Name":"ProductOrder","TypeParameter":"Range","Values":[ниже структуры],"Limit":0,"Offset":0}]}
+
+{"Table":"ProductOrder","Query":"Read"}
+{"Table":"PromotionsTypes","Query":"Read","Limit":999}
+{"Table":"Promotions","Query":"Read","Limit":999}
+{"Table":"Subjects","Query":"Read","Limit":999}
+{"Table":"ProductOrder","Query":"Read","TypeParameter":"TypeParameter","Values":["Value"]}")
